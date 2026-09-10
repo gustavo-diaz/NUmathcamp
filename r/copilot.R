20f2e8cb264ad99849c5411ba66e9acdcfda56f1
gustavo-diaz <gustavo.diaz@northwestern.edu>
2026-09-10 15:51:03 -0500

    Adding github page

---
+++ r/copilot.R
@@ -0,0 +1,11 @@
+# This script will load data from the World Development Indicators
+# Then estimate the relationship between
+# access to clean water and infant mortality
+# using the lm_robust function from the estimatr package
+# including gdp per capita as a control variable
+
+
+
+
+
+
